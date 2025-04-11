be7fdbbf53f254156b9f86de37cf45c6206330f5
jciura <jciura@student.agh.edu.pl>
2025-04-11 10:10:40 +0200

    Initial commit

---
+++ src/main/java/pl/agh/edu/io/User/User.java
@@ -0,0 +1,9 @@
+package pl.agh.edu.io.User;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="User")
+public class User {
+
+}
---
+++ src/main/java/pl/agh/edu/io/User/UserController.java
@@ -0,0 +1,4 @@
+package pl.agh.edu.io.User;
+
+public class UserController {
+}
