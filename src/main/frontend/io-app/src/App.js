6d96e566e909ccebefb8cb64046dc21383f9ec8b
pajka2T <tom16lolo@gmail.com>
2025-05-13 13:49:42 +0200

    Import fix in App.js

--- src/main/frontend/io-app/src/App.js
+++ src/main/frontend/io-app/src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import LoginForm from "./LoginForm";
 import Layout from "./Layout";
